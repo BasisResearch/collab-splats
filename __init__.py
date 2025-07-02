091d7b885b6d2985d47ca29a61bc83d2475b3691
root <tlasmanbotch@gmail.com>
2025-07-02 18:14:24 +0000

    updating w/ link to cpp extension

--- __init__.py
+++ __init__.py
@@ -3,8 +3,10 @@
 __version__ = "0.0.1"
 
 from .rade_gs.models import RadegsModel, RadegsModelConfig
+from .rade_gs.submodules.tetra_triangulation.tetranerf.utils.extension import cpp
 
 __all__ = [
     "RadegsModel",
     "RadegsModelConfig",
+    "cpp",
 ]

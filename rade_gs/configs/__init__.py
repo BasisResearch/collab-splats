3ac433e098b8b97f835338e31988cb9710219115
root <tlasmanbotch@gmail.com>
2025-07-03 15:16:18 +0000

    trying to fix the circular import

--- rade_gs/configs/__init__.py
+++ rade_gs/configs/__init__.py
@@ -1,10 +1,10 @@
 # from .rade_gs_config import RaDeGSModelConfig
 
 from .rade_gs_method import rade_gs_method
-from .trainer_config import _TrainerConfig, _ExperimentConfig
+# from .trainer_config import _TrainerConfig, _ExperimentConfig
 
 __all__ = [
     "rade_gs_method", 
-    "_TrainerConfig", 
-    "_ExperimentConfig"
+    # "_TrainerConfig", 
+    # "_ExperimentConfig"
 ]

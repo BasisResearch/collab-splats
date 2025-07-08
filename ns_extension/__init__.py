f8ebf9a37696b3c7464f36a79c8743af33d48600
tommybotch <tlasmanbotch@gmail.com>
2025-07-08 12:00:41 -0400

    updating adding back in marching tetras

--- ns_extension/__init__.py
+++ ns_extension/__init__.py
@@ -3,14 +3,14 @@
 __version__ = "0.0.1"
 
 from ns_extension.models.rade_gs_model import RadegsModel, RadegsModelConfig
-# from ns_extension.submodules.tetra_triangulation.tetranerf.utils.extension import cpp
+from ns_extension.submodules.tetra_triangulation.tetranerf.utils.extension import cpp
 from ns_extension.utils.camera_utils import ColmapCamera, convert_to_colmap_camera, depth_double_to_normal
 from ns_extension.utils.trainer_config import _TrainerConfig, _ExperimentConfig
 
 __all__ = [
     "RadegsModelConfig",
     "RadegsModel",
-    # "cpp",
+    "cpp",
     "ColmapCamera",
     "convert_to_colmap_camera",
     "depth_double_to_normal",

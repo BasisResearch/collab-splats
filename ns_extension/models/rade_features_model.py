bd75c068f591fb55c20a1330bac6def18683d515
root <tlasmanbotch@gmail.com>
2025-07-10 03:15:42 +0000

    will this setup fix

--- ns_extension/models/rade_features_model.py
+++ ns_extension/models/rade_features_model.py
@@ -80,9 +80,11 @@ class RadegsFeaturesModel(RadegsModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        super().populate_modules()
 
         # Moving this from populate models to here because we need 
         # the device initialized before we can populate text encoder
+        
         # Initialize per-Gaussian features
         distill_features = torch.nn.Parameter(torch.zeros((self.means.shape[0], self.config.features_latent_dim)))
         self.gauss_params["distill_features"] = distill_features
--- ns_extension/models/rade_gs_model.py
+++ ns_extension/models/rade_gs_model.py
@@ -55,6 +55,10 @@ class RadegsModel(SplatfactoModel):
 
     config: RadegsModelConfig
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        super().populate_modules()
+
     # TODO: Override any potential functions/methods to implement your own method
     # or subclass from "Model" and define all mandatory fields.
     @property

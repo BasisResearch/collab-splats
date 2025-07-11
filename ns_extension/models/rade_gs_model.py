4d0c670fc81931d31f8db9a79926f4dd145b6dd2
root <tlasmanbotch@gmail.com>
2025-07-11 15:53:31 +0000

    i think the voxel mask is messing with things

--- ns_extension/models/rade_gs_model.py
+++ ns_extension/models/rade_gs_model.py
@@ -133,7 +133,7 @@ class RadegsModel(SplatfactoModel):
         camera_params = self._get_camera_parameters(camera)
 
         # Get visible gaussian mask
-        voxel_visible_mask = self._prefilter_voxel(camera_params)
+        # voxel_visible_mask = self._prefilter_voxel(camera_params)
         
         # apply the compensation of screen space blurring to gaussians
         if self.config.rasterize_mode not in ["antialiased", "classic"]:
@@ -169,7 +169,7 @@ class RadegsModel(SplatfactoModel):
             colors=colors_crop,
             render_mode=render_mode,
             sh_degree_to_use=sh_degree_to_use,
-            visible_mask=voxel_visible_mask,
+            # visible_mask=voxel_visible_mask,
             camera_params=camera_params,
         )
 

0e2130bc8966eb3c40d8ea9c0aedfc5dd741fe40
Tommy <tlasmanbotch@gmail.com>
2026-04-16 21:03:22 +0000

    refactor: wire nerfstudio/__init__.py public API

---
+++ collab_splats/nerfstudio/__init__.py
@@ -0,0 +1,10 @@
+from .models import RadegsModel, RadegsModelConfig, RadegsFeaturesModel, RadegsFeaturesModelConfig
+from .model_loading import load_checkpoint
+
+__all__ = [
+    "RadegsModel",
+    "RadegsModelConfig",
+    "RadegsFeaturesModel",
+    "RadegsFeaturesModelConfig",
+    "load_checkpoint",
+]

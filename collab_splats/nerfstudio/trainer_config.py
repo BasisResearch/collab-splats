453bcc285de8ceadb41046a7f3141ec6d54358c3
Tommy <tlasmanbotch@gmail.com>
2026-04-16 20:28:48 +0000

    refactor: move trainer_config and model_loading into nerfstudio/

---
+++ collab_splats/nerfstudio/trainer_config.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Literal
+
+from nerfstudio.configs.experiment_config import ExperimentConfig
+from nerfstudio.engine.trainer import TrainerConfig
+
+
+@dataclass
+class _ExperimentConfig(ExperimentConfig):
+    """Patched experiment config to disable viewer."""
+
+    vis: Literal[
+        "viewer",
+        "wandb",
+        "tensorboard",
+        "comet",
+        "viewer+wandb",
+        "viewer+tensorboard",
+        "viewer+comet",
+        "viewer_legacy",
+        None,
+    ] = None
+    """Which visualizer to use."""
+
+
+class _TrainerConfig(TrainerConfig):
+    """Patched trainer config to disable viewer."""
+
+    vis: Literal[
+        "viewer",
+        "wandb",
+        "tensorboard",
+        "comet",
+        "viewer+wandb",
+        "viewer+tensorboard",
+        "viewer+comet",
+        "viewer_legacy",
+        None,
+    ] = None
+    """Which visualizer to use."""
